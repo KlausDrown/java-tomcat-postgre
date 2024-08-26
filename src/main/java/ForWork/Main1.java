f9735a6bb28cfa37094fb2cc9f26cdeb8a76dc58
KlausDrown <klausdrown@gmail.com>
2024-08-26 03:47:33 +0300

    проект с работающим doGet

---
+++ src/main/java/ForWork/Main1.java
@@ -0,0 +1,34 @@
+package ForWork;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "first", value = "")
+public class Main1 extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        System.out.println("INIT");
+    }
+
+        @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        var writer = resp.getWriter();
+        writer.println("helloworld.first");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("DESTROY");
+    }
+}
